2d1fd44dd1a4998bcf169a00cdf343c41085427e
novakvova <novakvova@gmail.com>
2017-11-12 13:22:07 +0200

    Begin Simple test

---
+++ CalcTest/CaclUnitTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Calculator;
+
+namespace CalcTest
+{
+    [TestClass]
+    public class CaclUnitTest
+    {
+        [TestMethod]
+        public void TestAdd()
+        {
+            Calculation calc = new Calculation();
+            int result = Calculation.Add(23,0);
+            Assert.AreEqual(result, 23);
+            result = Calculation.Add(34, 23);
+            Assert.AreEqual(result, 57);
+        }
+    }
+}
---
+++ CalcTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CalcTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CalcTest")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("efe59e69-1b36-4975-ab6e-1470685d9222")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
